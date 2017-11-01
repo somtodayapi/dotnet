4d662f6d0ed6d3aff00fa0615f973bedaa4f010a
Christos Karapasias <myproductsofficial@outlook.com>
2017-11-01 23:12:48 +0100

    Added updated test env.

---
+++ TestEnv1/MainPage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TestEnv1.ViewModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Globalization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestEnv1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel viewModel;
+        public MainPage()
+        {
+            ApplicationLanguages.PrimaryLanguageOverride = "nl";
+            this.InitializeComponent();
+            viewModel = new MainPageViewModel();
+
+        }
+        private bool isLoggedin()
+        {
+            bool bool1 = false;
+            return bool1;
+        }
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // you can also add items in code behind
+            NavView.MenuItems.Add(new NavigationViewItemSeparator());
+            NavView.MenuItems.Add(new NavigationViewItem()
+            { Content = "Vakken", Icon = new SymbolIcon(Symbol.Folder), Tag = "content" });
+
+            // set the initial SelectedItem 
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "home")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+            if (isLoggedin() == true)
+            {
+
+            }
+            else
+            {
+                gr.IsEnabled = false;
+                hw.IsEnabled = false;
+                sch.IsEnabled = false;
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(Views.Cijfer));
+            }
+            else
+            {
+                switch (args.InvokedItem)
+                {
+                    case "Home":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "cijfer":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "huiswerk":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "rooster":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "My content":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+                }
+            }
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                ContentFrame.Navigate(typeof(Views.Cijfer));
+            }
+            else
+            {
+
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+                switch (item.Tag)
+                {
+                    case "home":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "huiswerk":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "cijfers":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "music":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+
+                    case "content":
+                        ContentFrame.Navigate(typeof(Views.Cijfer));
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ TestEnv1/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.Globalization;
+
+namespace TestEnv1.ViewModel
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        ResourceLoader loader = new ResourceLoader("Strings");
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string title { get; set; }
+        private string grades { get; set; }
+        private string hw { get; set; }
+        private string sche { get; set; }
+        private string home { get; set; }
+        private string vakken { get; set; }
+
+
+        public MainPageViewModel()
+        {
+
+            Home = loader.GetString("home_str");
+            Grade = loader.GetString("grad_item");
+            Huiswerk = loader.GetString("hw_item");
+            Rooster = loader.GetString("sch_item");
+            Vakken = loader.GetString("vakk");
+
+            #region date
+            if (DateTime.Now.Hour < 12)
+            {
+                //Morning
+                if (isLoggedin() == true)
+                {
+                }
+                else
+                    Title = loader.GetString("goodmor") + ", " + loader.GetString("sign_in_com");           
+            }
+            else if (DateTime.Now.Hour < 17)
+            {
+                //Afternoon
+                if (isLoggedin() == true)
+                {
+                }
+                else
+                    Title = loader.GetString("goodaft") + ", " + loader.GetString("sign_in_com");
+            }
+            else
+            {
+                //Evening
+                if (isLoggedin() == true)
+                {
+                }
+                else
+                    Title = loader.GetString("goodev") + ", " + loader.GetString("sign_in_com");
+            }
+            #endregion
+        }
+        private bool isLoggedin()
+        {
+            bool bool1 = false;
+            return bool1;
+        }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #region strings
+        public string Title
+        {
+
+            get { return title; }
+            set
+            {
+                title = value;
+                NotifyPropertyChanged("Title");
+            }
+        }
+        public string Home
+        {
+
+            get { return home; }
+            set
+            {
+                home = value;
+                NotifyPropertyChanged("Home");
+            }
+        }
+        public string Grade
+        {
+
+            get { return grades; }
+            set
+            {
+                grades = value;
+                NotifyPropertyChanged("Grade");
+            }
+        }
+
+        public string Huiswerk
+        {
+
+            get { return hw; }
+            set
+            {
+                hw = value;
+                NotifyPropertyChanged("Huiswerk");
+            }
+        }
+        public string Rooster
+        {
+
+            get { return sche; }
+            set
+            {
+                sche = value;
+                NotifyPropertyChanged("Rooster");
+            }
+        }
+        public string Vakken
+        {
+
+            get { return vakken; }
+            set
+            {
+                vakken = value;
+                NotifyPropertyChanged("Vakken");
+            }
+        }
+        #endregion
+    }
+}
